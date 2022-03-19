59826c0047d6f49d69962268305ecffceb7f10a7
Niezzx <maxzzy224@outlook.com>
2022-03-19 12:39:52 -0700

    trouble onclick event/ advice -generator

---
+++ advice-generator-app-main/index.js
@@ -0,0 +1,17 @@
+function getAdvice() {
+	const xhr = new XMLHttpRequest();
+	/* initialize request */
+	xhr.open("GET", "https://api.adviceslip.com/advice");
+
+	xhr.onload = () => {
+		const data = JSON.parse(xhr.response);
+		const element = document.querySelector("h5");
+		element.children[0].innerHTML = data.slip.id;
+		element.children[1].innerHTML = data.slip.advice;
+	};
+
+	xhr.send();
+}
+
+const icon = document.querySelector(".icon");
+icon.onclick = getAdvice();
